1f9268f0fb8d7ce0717cc922ca43cafebecf6c5b
bri1- <bri1-purple@hotmail.com>
2017-02-27 20:03:29 -0600

    -Changed layouts -Added activities

--- app/src/main/java/team7/simon/MainActivity.java
+++ app/src/main/java/team7/simon/MainActivity.java
@@ -1,7 +1,11 @@
 package team7.simon;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,75 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b = (Button) findViewById(R.id.play);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        getApplicationContext(),
+                        SimonGameActivity.class
+                );
+                startActivity(intent);
+            }
+        });
+
+        b = (Button) findViewById(R.id.copyright);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        getApplicationContext(),
+                        aboutActivity.class
+                );
+                startActivity(intent);
+            }
+        });
+
+        b = (Button) findViewById(R.id.info);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("http://www.hasbro.com/common/documents/3f4e2ca0206011ddbd0b0800200c9a66/620962835056900B10D1688756D7BA4A.pdf");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        b = (Button) findViewById(R.id.version1);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        getApplicationContext(),
+                        simonSaysActivity.class
+                );
+                startActivity(intent);
+            }
+        });
+
+        b = (Button) findViewById(R.id.version2);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        getApplicationContext(),
+                        playerActivity.class
+                );
+                startActivity(intent);
+            }
+        });
+
+        b = (Button) findViewById(R.id.version3);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        getApplicationContext(),
+                        colorActivity.class
+                );
+                startActivity(intent);
+            }
+        });
     }
 }
